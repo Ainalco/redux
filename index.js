85bb5b70585e0faaac7874a469dc807a28576c31
Ainal <ainal1haque@gmail.com>
2023-09-04 22:22:23 +0600

    redux use store

--- index.js
+++ index.js
@@ -1,39 +1,57 @@
+const {createStore}= require("redux");
 
 //define constants
 const INCREMENT="INCREMENT";
 const DECREMENT="DECREMENT";
-const ADD_USER="add_User";
+
 
 // state
 const initialCounterState ={
-    count : 0
-}
-
-const initialUsersState ={
-    users : [{name:"Ainal Haque"}],
+    count : 0,
 }
 
 //action-Object-type,playload
 //Increment Counter
-const incrementCounterAction=()=>{
+const incrementCounter = () =>{
     return {
         type:INCREMENT,
     };
 };
 
-const decrementCounterAction=()=>{
+const decrementCounter=()=>{
     return {
         type:DECREMENT,
     };
 };
 
-const addUser=()=>{
-    return {
-        type:ADD_USER,
-        payload:{name:"Kamal"}
-    };
-};
+const counterReducer = (state=initialCounterState, action)=>{
+    switch (action.type){
+        case INCREMENT:
+            return {
+                ...state,
+                count: state.count + 1
+            }
+        
+        case DECREMENT:
+            return {
+                ...state,
+                count: state.count - 1
+            }
 
+        default:
+            state
+    }
+}
 
+//create Store
 
-//Decrement Counter
+const store = createStore(counterReducer);
+
+store.subscribe(()=>{
+    console.log(store.getState());
+});
+
+//dispatch action 
+store.dispatch(incrementCounter());
+store.dispatch(incrementCounter());
+store.dispatch(decrementCounter());
