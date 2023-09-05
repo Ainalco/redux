c34868204cd78f72acf4379123c7adbb421978c1
Ainal <ainal1haque@gmail.com>
2023-09-05 23:19:21 +0600

    redux complete counter end

--- index4.js
+++ index4.js
@@ -1,7 +1,9 @@
 
+const {createStore}= require("redux");
 
 const INCREMENT="INCREMENT";
 const DECREMENT="DECREMENT";
+const RESET="RESET";
 
 // state
 const initialState ={
@@ -19,3 +21,45 @@ const decrementCounter=()=>{
         type:DECREMENT,
     };
 };
+const resetCounter=()=>{
+    return {
+        type:RESET,
+    };
+};
+
+const counterReducer = (state=initialState, action)=>{
+    switch (action.type){
+        case INCREMENT:
+            return {
+                ...state,
+                count: state.count + 1
+            }
+        
+        case DECREMENT:
+            return {
+                ...state,
+                count: state.count - 1
+            }
+        case RESET:
+            return {
+                ...state,
+                count: 0
+            }
+
+        default:
+            state
+    }
+}
+
+const store = createStore(counterReducer);
+
+store.subscribe(()=>{
+    console.log(store.getState());
+});
+
+//dispatch action 
+store.dispatch(incrementCounter());
+store.dispatch(incrementCounter());
+store.dispatch(incrementCounter());
+store.dispatch(decrementCounter());
+store.dispatch(resetCounter());
